13e79d6f1c471e47bcf02dcd80b26b9cb9bce20d
blayzeaccount2020 <59625729+blayzeaccount2020@users.noreply.github.com>
2024-11-15 22:49:46 -0700

    fixed_speed.c with variable names identical to seen in stability testbench

--- fixed_speed.c
+++ fixed_speed.c
@@ -1,9 +1,12 @@
 int motor_pin = 10; // -> Operation Mode
-int extend_pin = 3; // -> IN A (Hardware PWM)
-int retract_pin = 4; // -> IN B
+int right_pin_roll = 3; // -> IN A (Hardware PWM)
+int left_pin_roll = 4; // -> IN B
+// int extend_pin = 3; // -> IN A (Hardware PWM)
+// int retract_pin = 4; // -> IN B
 
-int extend_sense = 8;
-int retract_sense = 9;
+
+int move_left_manual = 8;
+int move_right_manual = 9;
 
 // It's definitely this one, Blayze
 
@@ -23,8 +26,8 @@ void set_motor(direction_t dir)
   if (((dir == MOTOR_EXTEND) && (last_dir == MOTOR_RETRACT)) || ((dir == MOTOR_RETRACT) && (last_dir == MOTOR_EXTEND))
   {
     // Allow the motor to stop for a short period of time
-    digitalWrite(extend_pin, LOW);
-    digitalWrite(retract_pin, LOW);
+    digitalWrite(right_pin_roll, LOW);
+    digitalWrite(left_pin_roll, LOW);
     delay(100);
   }
 
@@ -32,29 +35,29 @@ void set_motor(direction_t dir)
   {
     case MOTOR_OFF:
     {
-      digitalWrite(extend_pin, LOW);
-      digitalWrite(retract_pin, LOW);
+      digitalWrite(right_pin_roll, LOW);
+      digitalWrite(left_pin_roll, LOW);
       break;
     }
 
     case MOTOR_EXTEND:
     {
-      digitalWrite(extend_pin, HIGH);
-      digitalWrite(retract_pin, LOW);
+      digitalWrite(right_pin_roll, HIGH);
+      digitalWrite(left_pin_roll, LOW);
       break;
     }
 
     case MOTOR_RETRACT:
     {
-      digitalWrite(extend_pin, LOW);
-      digitalWrite(retract_pin, HIGH);
+      digitalWrite(right_pin_roll, LOW);
+      digitalWrite(left_pin_roll, HIGH);
       break;
     }
 
     case MOTOR_BRAKE:
     {
-      digitalWrite(extend_pin, HIGH);
-      digitalWrite(retract_pin, HIGH);
+      digitalWrite(right_pin_roll, HIGH);
+      digitalWrite(left_pin_roll, HIGH);
       break;
     }
   }
@@ -66,16 +69,16 @@ void setup()
   digitalWrite(motor_pin, LOW);
 
   // Set INA / INB so the motor is off
-  digitalWrite(extend_pin, LOW);
-  digitalWrite(retract_pin, LOW);
+  digitalWrite(right_pin_roll, LOW);
+  digitalWrite(left_pin_roll, LOW);
 
   // Set Motor, Extend, and Retract as outputs
   pinMode(motor_pin, OUTPUT);
-  pinMode(extend_pin, OUTPUT);
-  pinMode(retract_pin, OUTPUT);
+  pinMode(right_pin_roll, OUTPUT);
+  pinMode(left_pin_roll, OUTPUT);
 
-  pinMode(extend_sense, INPUT);
-  pinMode(retract_sense, INPUT);
+  pinMode(move_left_manual, INPUT);
+  pinMode(move_right_manual, INPUT);
 }
 
 void loop()
@@ -88,8 +91,8 @@ void loop()
   //set_motor(MOTOR_RETRACT);
   //delay(900);
 
-  int extend = digitalRead(extend_sense);
-  int retract = digitalRead(retract_sense);
+  int extend = digitalRead(move_left_manual);
+  int retract = digitalRead(move_right_manual);
 
   if (extend == LOW)
   {
